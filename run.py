2909a506e5b6ddb4735450c6a3d7043d1887b725
Valerio Solinas <valerius92@live.it>
2024-06-03 18:47:05 +0000

    Create grid structure and initialized with main function

--- run.py
+++ run.py
@@ -1,3 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#module imports
+import random
+
+
+def make_grid(size):
+    """
+    Create a grid with "~" based on size choosen
+    """
+    return [["~"] * size for i in range(size)]
+
+def print_grid(grid):
+    """
+    Printing the grid using join() for better readability
+    Using a second print for spacing
+    """
+    for row in grid:
+        print(" ".join(row))
+    print()
+
+def game():
+        """
+        Initialize main function
+        """
+        while True:
+            try:
+                choose_size = int(input("Choose the board size: 1 for small, 2 for large: "))
+                if choose_size == 1:
+                    size = 5
+                    break
+                elif choose_size == 2:
+                    size = 10
+                    break
+                else:
+                    print("Invalid choice! Please enter 1 or 2.")
+            except ValueError:
+                print("Invalid choise! Please enter 1 or 2.")
+        grid = make_grid(size)
+        print(f"This is your {size}x{size} board!")  
+        print_grid(grid)
+
+
+game()
