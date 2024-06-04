8ce85c5a65d9a5bb9c20cd6b0275269fd9436440
Valerio Solinas <valerius92@live.it>
2024-06-04 11:53:11 +0000

    add function to place a ship,add function to place all ships,update game function

--- run.py
+++ run.py
@@ -16,6 +16,27 @@ def print_grid(grid):
     for row in grid:
         print(" ".join(row))
     print()
+
+def place_a_ship(grid, row, col):
+    """
+    Placing a signle ship 
+    """
+    grid[row][col] = "00"
+
+def place_all_ships(grid, ships_num):
+    """
+    Placing all the ships in the grid, ship size is 1 
+    """
+    size = len(grid)
+    placed_ships = 0
+    while placed_ships < ships_num:
+        row = random.randint(0, size - 1)
+        col = random.randint(0, size - 1)
+        if grid[row][col] == "~~":
+            place_a_ship(grid,row,col)
+            placed_ships +=1
+
+
 # initialize game letting the user choose between two grids
 def game():
         """
@@ -36,6 +57,11 @@ def game():
                 print("Invalid choise! Please enter 1 or 2.")
         grid_palyer = make_grid(size)
         grid_cpu = make_grid(size)
+        # number of ships that i want randomly placed
+        ships_num = 5
+        #Placing ships related to player/cpu grip and number of ships wanted
+        place_all_ships(grid_palyer,ships_num)
+        place_all_ships(grid_cpu,ships_num)
         print(f"The {size}x{size} board is ready!")  
         print_grid(grid_palyer)
         print_grid(grid_cpu)
